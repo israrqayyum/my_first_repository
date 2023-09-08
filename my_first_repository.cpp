7bc74ce1c343f4c73b1381ef3b43dc83404d031d
Israr-Qayyum <israrqayyum7@gmail.com>
2023-09-08 19:35:03 +0500

    This is initial commit

---
+++ my_first_repository.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+//header file for input and output
+ using namespace std;
+//using standard library 
+ int main(){
+ 	cout<<"--------Hello World --------"<<endl;
+ 	//display on output screen
+ 	cout<<"This is my first github repository"<<endl;
+ 	//display on output screen
+	return 0;
+ }
